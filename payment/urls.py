89a849e329d940047ce05879dd45cefb9ef3c5ea
Victor Oluwamakin <victor.oluwamakin@gmail.com>
2024-12-11 14:45:02 -0600

    added paypal payments3

--- payment/urls.py
+++ payment/urls.py
@@ -4,6 +4,8 @@ from django.conf import settings
 from django.conf.urls.static import static
 from .views import process_order
 
+app_name = 'payment'
+
 urlpatterns = [
     path('payment_success', views.payment_success, name='payment_success'),
     path('payment_failed', views.payment_failed, name='payment_failed'),
