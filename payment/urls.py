4a6df6934ab9467e3e5e3df4db14eded379814a3
Victor Oluwamakin <victor.oluwamakin@gmail.com>
2024-12-11 14:33:29 -0600

    added paypal payments2

--- payment/urls.py
+++ payment/urls.py
@@ -6,7 +6,7 @@ from .views import process_order
 
 urlpatterns = [
     path('payment_success', views.payment_success, name='payment_success'),
-    path('payment:failed', views.payment_failed, name='payment_failed'),
+    path('payment_failed', views.payment_failed, name='payment_failed'),
     path('checkout', views.checkout, name='checkout'),
     path('billing_info', views.billing_info, name='billing_info'),
     path('process_order', views.process_order, name='process_order'),
