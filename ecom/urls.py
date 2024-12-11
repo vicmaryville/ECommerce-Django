2f9997ab3d32079cf88c825bde7d49a8641e42b6
Victor Oluwamakin <victor.oluwamakin@gmail.com>
2024-12-11 15:22:31 -0600

    added paypal payments

--- ecom/urls.py
+++ ecom/urls.py
@@ -7,7 +7,7 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("Store.urls")),
     path("cart/", include("cart.urls")),
-    path("payment/", include("payment.urls")),
+    path("payment/", include("payment.standard.ipn.urls")),
    
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
